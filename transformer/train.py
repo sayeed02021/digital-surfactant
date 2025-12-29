780f1dfd9e3c138b84f728d074df80ca11b66772
sayeed02021 <106545515+sayeed02021@users.noreply.github.com>
2025-12-29 22:59:23 +0530

    added env.yml

---
+++ transformer/train.py
@@ -0,0 +1,16 @@
+import argparse
+
+import configuration.opts as opts
+from trainer.transformer_trainer import TransformerTrainer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='train.py',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    opts.train_opts(parser)
+    opt = parser.parse_args()
+    print(opt)
+
+    trainer = TransformerTrainer(opt)
+    trainer.train(opt)
